928df75f2b8ad7f86fe06c844883c1ed27e698e4
nancymei2011-collab <nancymei2011@gmail.com>
2026-06-29 07:38:03 +1200

    Publish DT website

---
+++ data.js
@@ -0,0 +1,144 @@
+const showcaseData = [
+  {
+    id: "image-creation",
+    navTitle: "Image Creation",
+    cardTitle: "Image Creation",
+    subtitle: "Character Creation",
+    image: "images/image-creation.jpg",
+    description: "These are all the cool images I made using Inkscape and Gimp.",
+    sections: [
+      {
+        title: "Character Creation",
+        subtitle: "A waving Duolingo",
+        text: " ",
+        image: "images/image-creation.jpg"
+      },
+      {
+        title: "Background Creation",
+        subtitle: "A beautiful sunrise",
+        text: " ",
+        image: "images/Background.png"
+      },
+      {
+        title: "Snowflakes",
+        subtitle: "A nice violet snowflake",
+        text: " ",
+        image: "images/computer-science.png"
+      },
+      {
+        title: "Pixel Art",
+        subtitle: "A delicious cake",
+        text: " ",
+        image: "images/python.jpg"
+      }
+    ]
+  },
+
+  {
+    id: "computer-science",
+    navTitle: "Computer Science",
+    cardTitle: "Computer Science",
+    subtitle: "Digital Technology Work",
+    image: "images/Binary Dragon.png",
+    description: "These are all the work I did through learning computer science.",
+    sections: [
+      {
+        title: "Hexadecimal",
+        subtitle: "Hex Colour Dragon",
+        text: " ",
+        image: "images/Binary Dragon.png"
+      },
+      {
+        title: "Error Detection",
+        subtitle: "Parity & Barcodes",
+        text: " ",
+        image: "images/Parity.png"
+      },
+      {
+        title: "ASCII",
+        subtitle: "A smiley face",
+        text: " ",
+        image: "images/ASCII Art.png"
+      }
+    ]
+  },
+
+  {
+    id: "python-programming",
+    navTitle: "Python Programming",
+    cardTitle: "Python Programming",
+    subtitle: "Coding Practice",
+    image: "images/Python.png",
+    description: "I have successfully finished learning Python on Grok!",
+    sections: [
+      {
+        title: "Python Programming",
+        subtitle: "Coding Practice",
+        text: "Python programming helped me learn problem solving, variables, loops, functions and logical thinking.",
+        image: "images/python.jpg"
+      }
+    ]
+  },
+
+  {
+    id: "3d-modelling",
+    navTitle: "3D Modelling",
+    cardTitle: "3D Modelling",
+    subtitle: "Blender Models",
+    image: "images/Donut.png",
+    description: "This section shows the 3D models I created in Blender.",
+    sections: [
+      {
+        title: "Donut",
+        subtitle: "Blender Model",
+        text: "This is a donut model I created in Blender using shapes, materials and lighting.",
+        image: "images/Donut.png"
+      },
+      {
+        title: "Mug with Hot Chocolate",
+        subtitle: "Blender Model",
+        text: "This model shows a mug of hot chocolate from another camera angle.",
+        image: "images/Mug with hot chocolate.png"
+      },
+      {
+        title: "Sword",
+        subtitle: "Blender Model",
+        text: "This is a sword model I created using Blender modelling tools.",
+        image: "images/Sword.png"
+      },
+      {
+        title: "City View",
+        subtitle: "Blender Model",
+        text: "This is a city view scene I created using Blender.",
+        image: "images/City view.png"
+      }
+    ]
+  },
+
+  {
+    id: "blender-animation",
+    navTitle: "Animation",
+    cardTitle: "Animation",
+    subtitle: "Cube Jumping Animation",
+    image: "images/blender logo.png",
+    description: "This section shows my Blender animation project.",
+    sections: [
+      {
+        title: "Cube Jumping Animation",
+        subtitle: "Animation",
+        text: "This is an animation I created in Blender.",
+        video: "videos/Cube Jumping Animation.mp4"
+      }
+    ]
+  },
+
+  {
+    id: "design-for-manufacturing",
+    navTitle: "D4M",
+    cardTitle: "D4M",
+    subtitle: "Design for Manufacturing",
+    image: "images/Website Header.png",
+    description: "Design for Manufacturing work will be added here.",
+    sections: []
+  }
+];
